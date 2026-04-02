f67f7dca52de1420358341abd56b81c78eb0d43e
Oliver Zott <oliver.zott@agindo.at>
2026-04-02 14:20:05 +0200

    mega example

--- csharp/playground.cs
+++ csharp/playground.cs
@@ -1,7 +1,20 @@
-Console.WriteLine(SayHello("Olli"));
 
 static string SayHello(string name)
 {
     var helloString = $"Hello {name}";
     return helloString;
 }
+
+static int SumInt(params int[] args)
+{
+    int result = 0;
+    foreach (var arg in args)
+    {
+        result += arg;
+    }
+    return result;
+}
+
+
+Console.WriteLine(SayHello("Olli"));
+Console.WriteLine(SumInt([3, 6, 91]));
